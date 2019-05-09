e7399d2fb02465088e7756c7e64ecca9521978d5
infernalfire72 <julius.tzunami@yahoo.de>
2019-05-09 21:25:48 +0200

    Reading Username and Passwort from Request Stream

--- Events/LoginEvent.cs
+++ Events/LoginEvent.cs
@@ -1,10 +1,28 @@
-﻿namespace Komorebi.Events
+﻿using Komorebi.Objects;
+using System.IO;
+using System.Net;
+
+namespace Komorebi.Events
 {
     public class LoginEvent
     {
-        public LoginEvent()
+        public static void Handle(ref HttpListenerContext ctx)
         {
-             
+            string Username, Password;
+
+            using (StreamReader r = new StreamReader(ctx.Request.InputStream))
+            {
+                Username = r.ReadLine();
+                Password = r.ReadLine();
+            }
+
+            // Check Login right here
+
+            //
+
+            Player p = new Player();
+
+            //ctx.Response.OutputStream.Write();
         }
     }
 }
