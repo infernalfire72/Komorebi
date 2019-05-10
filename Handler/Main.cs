44d8b2443a7e6975cb2a9bc2df5607d93246ac17
infernalfire72 <julius.tzunami@yahoo.de>
2019-05-10 13:05:56 +0200

    base packet handler

---
+++ Handler/Main.cs
@@ -0,0 +1,20 @@
+﻿using Colorful;
+using Komorebi.Objects;
+using Komorebi.Packets;
+using System.Drawing;
+
+namespace Komorebi.Handler
+{
+    public class Main
+    {
+        public static void Handle(Player p, Packet packet)
+        {
+            switch(packet.Type)
+            {
+                default:
+                    Console.WriteLineFormatted($"Got Unknown Packet {packet.Type} with Length {packet.Data?.Length ?? 0}", Color.Yellow);
+                    break;
+            }
+        }
+    }
+}
