339b55e1ae086cfb27cfe3edf806d4074c4eb0b8
infernalfire72 <julius.tzunami@yahoo.de>
2019-05-11 14:07:07 +0200

    Channels

--- Objects/Channel.cs
+++ Objects/Channel.cs
@@ -1,26 +1,34 @@
 ﻿using Komorebi.Packets;
+using MySql.Data.MySqlClient;
+using Shared;
+using System.Collections.Generic;
 
 namespace Komorebi.Objects
 {
     public class Channel : ISerializable
     {
         public string Name, Topic;
-        public short UserCount;
+        public short UserCount => (short)joinedPlayers.Count;
+
+        public bool Write = true;
+        public bool Read = true;
+
+
+        public List<Player> joinedPlayers = new List<Player>();
 
         public Channel() { }
         public Channel(byte[] data) => this.Populate(data);
-        public Channel(string Name, string Topic, short Users)
+        public Channel(string Name, string Topic)
         {
             this.Name = Name;
             this.Topic = Topic;
-            this.UserCount = Users;
         }
 
         public void ReadFromStream(PacketReader r)
         {
             Name = r.ReadString();
             Topic = r.ReadString();
-            UserCount = r.ReadInt16();
+            //UserCount = r.ReadInt16();
         }
 
         public void WriteToStream(PacketWriter w)
@@ -28,6 +36,30 @@ namespace Komorebi.Objects
             w.Write(Name);
             w.Write(Topic);
             w.Write(UserCount);
+        }
+    }
+
+    public class ChannelList
+    {
+        private static bool a = false;
+
+        public static void Initialize()
+        {
+            if(a)
+            {
+                Global.Channels.Clear();
+            }
+            using (MySqlDataReader r = Database.RunQuery("SELECT * FROM channels;"))
+            {
+                while(r.Read())
+                {
+                    Channel c = new Channel(r.GetString("name"), r.GetString("topic"));
+                    c.Write = r.GetByte("public_write") == 1;
+                    c.Read = r.GetByte("public_read") == 1;
+                    Global.Channels.Add(c.Name, c);
+                }
+            }
+            a = true;
         }
     }
 }
--- Objects/Global.cs
+++ Objects/Global.cs
@@ -6,5 +6,6 @@ namespace Komorebi
     public static class Global
     {
         public static Dictionary<string, Player> Players = new Dictionary<string, Player>();
+        public static Dictionary<string, Channel> Channels = new Dictionary<string, Channel>();
     }
 }
